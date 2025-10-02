aed9a7460887284797c22c6e75f21dcb8c17df13
RHFridge <rhfridge@github.com>
2025-10-02 11:38:36 +0100

    5 - Variables 1&2

---
+++ Tutorials/C/5 - Variables/create_variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    /* 
+    Variables follow the format:
+
+    type variableName = value;
+    */
+
+    int myNum = 15;
+
+    /*
+    You can also declare a variable without a value:
+
+    type variableName;
+    */
+
+    int myOtherNum;
+    myOtherNum = 14;
+}
---
+++ Tutorials/C/5 - Variables/format_specifiers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+Format specifiers start with a %
+*/
+
+int main (){
+
+    int myNum = 15;
+    float myFloatNum = 5.99;
+    char myLetter = 'D';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    return 0;
+}
