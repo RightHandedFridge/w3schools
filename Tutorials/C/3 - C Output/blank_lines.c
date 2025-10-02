5cce4cbc26d1b33925031490a1dfa89e78cf9526
RHFridge <rhfridge@github.com>
2025-10-02 11:01:45 +0100

    3 - C Output

---
+++ Tutorials/C/3 - C Output/blank_lines.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Yo Waddup\n\n");
+    printf("I am learning C.");
+}
+
+/*
+Outputs:
+"Yo Waddup
+
+I am learning C."
+
+Using two or \n creates a single blank line
+*/
---
+++ Tutorials/C/3 - C Output/new_lines.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Yo Waddup\n");
+    printf("I am learning C.");
+}
+
+/*
+Outputs:
+"Yo Waddup
+I am learning C."
+
+\n starts a new line
+*/
