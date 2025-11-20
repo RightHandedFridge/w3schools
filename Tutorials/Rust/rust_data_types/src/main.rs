4e25c29e1b2d9602d9d9c6198e3654c0ce440906
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2025-11-20 14:53:33 +0000

    Start data types

--- Tutorials/Rust/rust_data_types/src/main.rs
+++ Tutorials/Rust/rust_data_types/src/main.rs
@@ -1,3 +1,28 @@
+//Required to use Hashmaps
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    //Arrays
+    // Has a fixed size, index starts at 0
+    let fruits_array = ["apple", "banana", "orange"];
+    println!("Last Fruit: {}", fruits_array[2]);
+
+    //Vectors
+    let mut fruits_vector = vec!["apple", "banana"];
+    fruits_vector.push("cherry");
+    println!("Last Fruit: {}", fruits_vector[2]);
+
+    //Tuples
+    // Can store multiple different data types
+    let person = ("John", 30, true);
+    println!("Name: {}", person.0);
+    println!("Age: {}", person.1);
+    println!("Is active: {}", person.2);
+
+    //Hashmaps
+    let mut capitalCities = HashMap::new();
+    capitalCities.insert("France", "Paris");
+    capitalCities.insert("Japan", "Tokyo");
+
+    println!("Capital of Japan: {}", capitalCities["Japan"]);
 }
