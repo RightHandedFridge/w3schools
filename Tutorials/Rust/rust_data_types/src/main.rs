1d2e39d9f09d0c664f1671484dd046bf246c6924
RHFridge <28656853+RightHandedFridge@users.noreply.github.com>
2025-11-21 08:25:46 +0000

    Rust: Arrays and Vectors

--- Tutorials/Rust/rust_data_types/src/main.rs
+++ Tutorials/Rust/rust_data_types/src/main.rs
@@ -6,11 +6,38 @@ fn main() {
     // Has a fixed size, index starts at 0
     let fruits_array = ["apple", "banana", "orange"];
     println!("Last Fruit: {}", fruits_array[2]);
+    // Arrays must have the "mut" keyword if a value inside them is to be changed at runtime
+    // fruits_array[0] = "strawberry"; wouldn't work
+    let mut numbers = [1, 2, 3, 4, 5];
+    numbers[0] = 10;
+    println!("The new first number is: {}", numbers[0]);
+    println!("This array has {} elements", numbers.len());
+
+    //Loop through array
+    for fruit in fruits {
+        println!("I like {}.", fruit);
+    }
+
+    //Print whole array
+    println!("{:?}", numbers);
+    
 
     //Vectors
+    // the vec! macro denotes what is a vector
     let mut fruits_vector = vec!["apple", "banana"];
-    fruits_vector.push("cherry");
+    //Vectors are the same as arrays except they are dynamic
+    // so they can increase or decrease in size
+    fruits_vector.push("cherry"); //Add to the end
+    fruits_vector.pop(); //Remove from the end
+    fruits_vector.insert(0, "apple"); //Add at index
+    fruits_vector.remove(0); //Remove from index
+    println!("There are {} fruits", fruits_vector.len());
     println!("Last Fruit: {}", fruits_vector[2]);
+
+    //Loop through vector
+    for fruit in &fruits_vector { //Must use & here to borrow the data rather than stealing it
+        println!("I like {}", fruit);
+    }
 
     //Tuples
     // Can store multiple different data types
